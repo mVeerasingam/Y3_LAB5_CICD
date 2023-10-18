6acfcaf679c4638376812d633a01f0d7c381003f
Mark Veerasingam <markveer70@gmail.com>
2023-10-18 14:37:49 +0100

    first commit

---
+++ src/main/java/ie/atu/y3_lab5_cicd/Y3Lab5CicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.y3_lab5_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y3Lab5CicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Y3Lab5CicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/y3_lab5_cicd/Y3Lab5CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.y3_lab5_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Y3Lab5CicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
