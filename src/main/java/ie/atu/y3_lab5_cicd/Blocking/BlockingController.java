d6612d6bf450a1dd442a8bda93a016ef808daf8d
Mark Veerasingam <markveer70@gmail.com>
2023-10-18 15:45:36 +0100

    made a blcking example package

--- src/main/java/ie/atu/y3_lab5_cicd/Blocking/BlockingController.java
+++ src/main/java/ie/atu/y3_lab5_cicd/Blocking/BlockingController.java
@@ -1,4 +1,4 @@
-package ie.atu.y3_lab5_cicd;
+package ie.atu.y3_lab5_cicd.Blocking;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
