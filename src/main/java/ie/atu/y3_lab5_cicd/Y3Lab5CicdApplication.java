3f358e572291eb09e8dbc4fa84b2c6e06cad10b0
Mark Veerasingam <markveer70@gmail.com>
2023-10-18 14:40:10 +0100

    added beans

--- src/main/java/ie/atu/y3_lab5_cicd/Y3Lab5CicdApplication.java
+++ src/main/java/ie/atu/y3_lab5_cicd/Y3Lab5CicdApplication.java
@@ -2,12 +2,17 @@ package ie.atu.y3_lab5_cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Y3Lab5CicdApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Y3Lab5CicdApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate restTemplate() {return new RestTemplate();}
 }
